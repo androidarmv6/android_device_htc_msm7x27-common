d39b42e857e44b76d6ff3dea7fba60b7068439fd
Olivier <y.karasangabo@bg-rams.ac.at>
2014-04-30 21:33:55 +0200

    marvel: use generic graphics
    May be reverted later on

--- recovery/BoardConfigCommon.mk
+++ recovery/BoardConfigCommon.mk
@@ -5,7 +5,7 @@ TARGET_RECOVERY_INITRC = device/htc/msm7x27-common/recovery/init.htc.rc
 #BOARD_USES_RECOVERY_CHARGEMODE := true
 BOARD_USES_MMCUTILS := false
 
-BOARD_CUSTOM_GRAPHICS := ../../../device/htc/msm7x27-common/recovery/graphics.c
+#BOARD_CUSTOM_GRAPHICS := ../../../device/htc/msm7x27-common/recovery/graphics.c
 
 BOARD_HAS_NO_SELECT_BUTTON := true
 BOARD_HAS_NO_MISC_PARTITION := true
