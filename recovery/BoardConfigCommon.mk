a9434108a89de62994de01f7a34d3750f11baad3
Olivier <y.karasangabo@bg-rams.ac.at>
2014-05-03 19:23:06 +0200

    msm7x27: fixup for CWM v6
    It now works on the Marvel. :)

--- recovery/BoardConfigCommon.mk
+++ recovery/BoardConfigCommon.mk
@@ -1,11 +1,14 @@
 BOARD_USES_MSM7X27_RECOVERY := true
-
-TARGET_RECOVERY_INITRC = device/htc/msm7x27-common/recovery/init.htc.rc
-
-#BOARD_USES_RECOVERY_CHARGEMODE := true
 BOARD_USES_MMCUTILS := false
 
-#BOARD_CUSTOM_GRAPHICS := ../../../device/htc/msm7x27-common/recovery/graphics.c
+BOARD_CUSTOM_GRAPHICS := ../../../device/htc/msm7x27-common/recovery/graphics.c
 
 BOARD_HAS_NO_SELECT_BUTTON := true
 BOARD_HAS_NO_MISC_PARTITION := true
+
+BOARD_USE_CUSTOM_RECOVERY_FONT := \"font_7x16.h\"
+BOARD_RECOVERY_HANDLES_MOUNT := true
+
+TARGET_NO_INITLOGO := true
+TARGET_RECOVERY_PIXEL_FORMAT := "RGB_565"
+TARGET_RECOVERY_LCD_BACKLIGHT_PATH := \"/sys/class/leds/lcd-backlight/brightness\"
