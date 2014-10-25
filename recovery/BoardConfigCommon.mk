57e48c247d5d3d9adea77b973ad3c45142ef13b5
olivieer <yves.olli@gmail.com>
2014-10-25 15:23:18 -0400

    recovery: Restore init.htc.rc
    Change-Id: Ie83ba219179c4fdcfe2d1a4d7689c834ea5798c5

--- recovery/BoardConfigCommon.mk
+++ recovery/BoardConfigCommon.mk
@@ -1,6 +1,8 @@
 BOARD_USES_MSM7X27_RECOVERY := true
 BOARD_USES_MMCUTILS := false
 
+TARGET_RECOVERY_INITRC = device/htc/msm7x27-common/recovery/init.htc.rc
+
 BOARD_CUSTOM_GRAPHICS := ../../../device/htc/msm7x27-common/recovery/graphics.c
 
 BOARD_HAS_NO_SELECT_BUTTON := true
